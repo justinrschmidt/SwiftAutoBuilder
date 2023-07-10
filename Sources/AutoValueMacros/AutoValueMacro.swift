9d2e5daf2b86199f18e876fb209fc92307aa265c
schimdt.cpp <schmidt.cpp@gmail.com>
2023-07-10 17:28:15 -0400

    convert from tabs to spaces to reduce whitespace related mistakes in code expansion tests

--- Sources/AutoValueMacros/AutoValueMacro.swift
+++ Sources/AutoValueMacros/AutoValueMacro.swift
@@ -4,44 +4,44 @@ import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
 enum AutoValueError: CustomStringConvertible, Error {
-	case invalidType
+    case invalidType
 
-	var description: String {
-		switch self {
-		case .invalidType:
-			return "@AutoValue can only be applied to structs"
-		}
-	}
+    var description: String {
+        switch self {
+        case .invalidType:
+            return "@AutoValue can only be applied to structs"
+        }
+    }
 }
 
 public struct AutoValueMacro: MemberMacro {
-	public static func expansion(
-		of node: AttributeSyntax,
-		providingMembersOf declaration: some DeclGroupSyntax,
-		in context: some MacroExpansionContext) throws -> [DeclSyntax] {
-			if let structDecl = declaration.as(StructDeclSyntax.self) {
-				return try expandStruct(structDecl, of: node, in: context)
-			} else {
-				throw AutoValueError.invalidType
-			}
-		}
+    public static func expansion(
+        of node: AttributeSyntax,
+        providingMembersOf declaration: some DeclGroupSyntax,
+        in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+            if let structDecl = declaration.as(StructDeclSyntax.self) {
+                return try expandStruct(structDecl, of: node, in: context)
+            } else {
+                throw AutoValueError.invalidType
+            }
+        }
 
-	private static func expandStruct(
-		_ structDecl: StructDeclSyntax,
-		of node: AttributeSyntax,
-		in context: some MacroExpansionContext) throws -> [DeclSyntax] {
-			return [
-				try InitializerDeclSyntax("init(with builder: Builder)", bodyBuilder: {
-				}).as(DeclSyntax.self)!,
-				try ClassDeclSyntax("class Builder", membersBuilder: {
-				}).as(DeclSyntax.self)!
-			]
-		}
+    private static func expandStruct(
+        _ structDecl: StructDeclSyntax,
+        of node: AttributeSyntax,
+        in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+            return [
+                try InitializerDeclSyntax("init(with builder: Builder)", bodyBuilder: {
+                }).as(DeclSyntax.self)!,
+                try ClassDeclSyntax("class Builder", membersBuilder: {
+                }).as(DeclSyntax.self)!
+            ]
+        }
 }
 
 @main
 struct AutoValuePlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-		AutoValueMacro.self,
+        AutoValueMacro.self,
     ]
 }
--- Tests/AutoValueTests/AutoValueTests.swift
+++ Tests/AutoValueTests/AutoValueTests.swift
@@ -4,99 +4,99 @@ import XCTest
 import AutoValueMacros
 
 let testMacros: [String: Macro.Type] = [
-	"AutoValue": AutoValueMacro.self,
+    "AutoValue": AutoValueMacro.self,
 ]
 
 final class AutoValueTests: XCTestCase {
-	func testEmptyStruct() {
-		assertMacroExpansion(
-			"""
-			@AutoValue
-			struct Foo {
-			}
-			""",
-			expandedSource:
-			"""
-			struct Foo {
-			    init(with builder: Builder) {
-			    }
-			    class Builder {
-			    }
-			}
-			""", macros: testMacros)
-	}
+    func testEmptyStruct() {
+        assertMacroExpansion(
+            """
+            @AutoValue
+            struct Foo {
+            }
+            """,
+            expandedSource:
+            """
+            struct Foo {
+                init(with builder: Builder) {
+                }
+                class Builder {
+                }
+            }
+            """, macros: testMacros)
+    }
 
-	func testStructWithStoredProperties() {
-		assertMacroExpansion(
-			"""
-			@AutoValue
-			struct Foo {
-				let a: Int
-				let b: Double
-			}
-			""",
-			expandedSource:
-			"""
-			struct Foo {
-				let a: Int
-				let b: Double
-				init(with builder: Builder) {
-				}
-				class Builder {
-				}
-			}
-			""", macros: testMacros)
-	}
+    func testStructWithStoredProperties() {
+        assertMacroExpansion(
+            """
+            @AutoValue
+            struct Foo {
+                let a: Int
+                let b: Double
+            }
+            """,
+            expandedSource:
+            """
+            struct Foo {
+                let a: Int
+                let b: Double
+                init(with builder: Builder) {
+                }
+                class Builder {
+                }
+            }
+            """, macros: testMacros)
+    }
 
-	func testStructWithComputedProperties() {
-		assertMacroExpansion(
-			"""
-			@AutoValue
-			struct Foo {
-				let a: Int
-				var b: Int {
-					get {
-						return a
-					}
-					set {
-						a = newValue
-					}
-				}
-			}
-			""",
-			expandedSource:
-			"""
-			struct Foo {
-				let a: Int
-				var b: Int {
-					get {
-						return a
-					}
-					set {
-						a = newValue
-					}
-				}
-				init(with builder: Builder) {
-				}
-				class Builder {
-				}
-			}
-			""", macros: testMacros)
-	}
+    func testStructWithComputedProperties() {
+        assertMacroExpansion(
+            """
+            @AutoValue
+            struct Foo {
+                let a: Int
+                var b: Int {
+                    get {
+                        return a
+                    }
+                    set {
+                        a = newValue
+                    }
+                }
+            }
+            """,
+            expandedSource:
+            """
+            struct Foo {
+                let a: Int
+                var b: Int {
+                    get {
+                        return a
+                    }
+                    set {
+                        a = newValue
+                    }
+                }
+                init(with builder: Builder) {
+                }
+                class Builder {
+                }
+            }
+            """, macros: testMacros)
+    }
 
-	func testInvalidType() {
-		assertMacroExpansion(
-			"""
-			@AutoValue
-			class Foo {
-			}
-			""",
-			expandedSource:
-			"""
-			class Foo {
-			}
-			""", diagnostics: [
-				DiagnosticSpec(message: "@AutoValue can only be applied to structs", line: 1, column: 1)
-			], macros: testMacros)
-	}
+    func testInvalidType() {
+        assertMacroExpansion(
+            """
+            @AutoValue
+            class Foo {
+            }
+            """,
+            expandedSource:
+            """
+            class Foo {
+            }
+            """, diagnostics: [
+                DiagnosticSpec(message: "@AutoValue can only be applied to structs", line: 1, column: 1)
+            ], macros: testMacros)
+    }
 }
