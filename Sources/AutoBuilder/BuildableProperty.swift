9282f086b70c52bef971225560423d94d369c88a
schimdt.cpp <schmidt.cpp@gmail.com>
2023-07-25 15:19:24 -0400

    create variants of BuildableProperty to use with arrays, dictionaries, and sets

--- Sources/AutoBuilder/BuildableProperty.swift
+++ Sources/AutoBuilder/BuildableProperty.swift
@@ -59,3 +59,89 @@ extension BuildableProperty where T: Buildable, T.Builder: BuilderProtocol, T.Bu
         }
     }
 }
+
+// MARK: - Collection Properties
+
+public class BuildableArrayProperty<Element> {
+    private var array: [Element]
+
+    public init() {
+        array = []
+    }
+
+    public func set(value: [Element]) {
+        array = value
+    }
+
+    public func append(element: Element) {
+        array.append(element)
+    }
+
+    public func append<C>(contentsOf collection: C) where C: Collection, C.Element == Element {
+        array.insert(contentsOf: collection, at: array.endIndex)
+    }
+
+    public func removeAll() {
+        array.removeAll()
+    }
+
+    public func build() -> [Element] {
+        return array
+    }
+}
+
+public class BuildableDictionaryProperty<Key, Value> where Key: Hashable {
+    private var dictionary: [Key:Value]
+
+    public init() {
+        dictionary = [:]
+    }
+
+    public func set(value: [Key:Value]) {
+        dictionary = value
+    }
+
+    public func insert(key: Key, value: Value) {
+        dictionary[key] = value
+    }
+
+    public func merge(other: [Key:Value], uniquingKeysWith combine: (Value, Value) throws -> Value) rethrows {
+        try dictionary.merge(other, uniquingKeysWith: combine)
+    }
+
+    public func removeAll() {
+        dictionary.removeAll()
+    }
+
+    public func build() -> [Key:Value] {
+        return dictionary
+    }
+}
+
+public class BuildableSetProperty<Element> where Element: Hashable {
+    private var set: Set<Element>
+
+    public init() {
+        set = []
+    }
+
+    public func set(value: Set<Element>) {
+        set = value
+    }
+
+    public func insert(element: Element) {
+        set.insert(element)
+    }
+
+    public func formUnion(other: Set<Element>) {
+        set.formUnion(other)
+    }
+
+    public func removeAll() {
+        set.removeAll()
+    }
+
+    public func build() -> Set<Element> {
+        return set
+    }
+}
