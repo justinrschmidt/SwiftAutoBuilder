bbe6a0cb20d524c0ff8aeaea4b61477f10743351
schimdt.cpp <schmidt.cpp@gmail.com>
2023-08-09 16:10:12 -0400

    add diagnostic warning for when an enum does not contain any cases with associated values

--- Sources/AutoBuilderMacros/AutoBuilderDiagnostic.swift
+++ Sources/AutoBuilderMacros/AutoBuilderDiagnostic.swift
@@ -5,6 +5,7 @@ public enum AutoBuilderDiagnostic: DiagnosticMessage {
     public static let domain = "AutoBuilderMacro"
 
     case impliedVariableType(identifier: String)
+    case noAssociatedValues(enumName: String)
     case invalidTypeForAutoBuilder
 
     public var severity: DiagnosticSeverity {
@@ -12,6 +13,8 @@ public enum AutoBuilderDiagnostic: DiagnosticMessage {
         case .impliedVariableType(_),
                 .invalidTypeForAutoBuilder:
             return .error
+        case .noAssociatedValues(_):
+            return .warning
         }
     }
 
@@ -19,6 +22,8 @@ public enum AutoBuilderDiagnostic: DiagnosticMessage {
         switch self {
         case .impliedVariableType(let identifier):
             return "Type annotation missing for '\(identifier)'. AutoBuilder requires all variable properties to have type annotations."
+        case .noAssociatedValues(let enumName):
+            return "\(enumName) does not have any cases with associated values."
         case .invalidTypeForAutoBuilder:
             return "@AutoBuilder can only be applied to structs"
         }
@@ -28,6 +33,8 @@ public enum AutoBuilderDiagnostic: DiagnosticMessage {
         switch self {
         case .impliedVariableType(_):
             return MessageID(domain: Self.domain, id: "ImpliedVariableType")
+        case .noAssociatedValues(_):
+            return MessageID(domain: Self.domain, id: "NoAssociatedValues")
         case .invalidTypeForAutoBuilder:
             return MessageID(domain: Self.domain, id: "InvalidTypeForAutoBuilder")
         }
