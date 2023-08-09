90f25cbe896e584da04fd004fa33f0567d14d5ff
schimdt.cpp <schmidt.cpp@gmail.com>
2023-08-09 16:10:38 -0400

    add error that is thrown when an enum builder does not have any case set

--- Sources/AutoBuilder/BuilderError.swift
+++ Sources/AutoBuilder/BuilderError.swift
@@ -4,4 +4,7 @@ public enum BuilderError: Error {
     /// Thrown when a value in one of a builder's properties is not set.
     /// - Parameter propertyName: The name of the property in the client type that this error is associated with.
     case missingValue(propertyName: String)
+
+    /// Thrown when a builder for an enum does not have a case set.
+    case noEnumCaseSet
 }
