637db3ced14d12284020bfcfb9ba6417572504b6
schimdt.cpp <schmidt.cpp@gmail.com>
2023-08-22 16:03:18 -0400

    add test for enum case with associated values with no labels

--- Tests/AutoBuilderMacroTests/AutoBuilderMacroEnumTests.swift
+++ Tests/AutoBuilderMacroTests/AutoBuilderMacroEnumTests.swift
@@ -393,4 +393,106 @@ final class AutoBuilderMacroEnumTests: XCTestCase {
         }
         """, macros: testMacros)
     }
+
+    func testEnumCaseWithNoLabels() {
+        assertMacroExpansion(
+            """
+            @AutoBuilder
+            enum Foo {
+                case one(Int, Double, String)
+            }
+            """,
+            expandedSource:
+            """
+            enum Foo {
+                case one(Int, Double, String)
+                init(with builder: Builder) throws {
+                    self = try builder.build()
+                }
+                func toBuilder() -> Builder {
+                    let builder = Builder()
+                    switch self {
+                    case let .one(i0, i1, i2):
+                        let oneBuilder = builder.one
+                        oneBuilder.set(i0: i0)
+                        oneBuilder.set(i1: i1)
+                        oneBuilder.set(i2: i2)
+                    }
+                    return builder
+                }
+                public class Builder: BuilderProtocol {
+                    private var currentCase: BuilderCases?
+                    public required init() {
+                        currentCase = nil
+                    }
+                    public var one: One {
+                        get {
+                            switch currentCase {
+                            case let .some(.one(builder)):
+                                return builder
+                            default:
+                                let builder = One()
+                                currentCase = .one(builder)
+                                return builder
+                            }
+                        }
+                        set {
+                            currentCase = .one(newValue)
+                        }
+                    }
+                    public func set(value: Foo) {
+                        switch value {
+                        case let .one(i0, i1, i2):
+                            let builder = One()
+                            builder.set(i0: i0)
+                            builder.set(i1: i1)
+                            builder.set(i2: i2)
+                            currentCase = .one(builder)
+                        }
+                    }
+                    public func build() throws -> Foo {
+                        switch currentCase {
+                        case let .some(.one(builder)):
+                            return try builder.build()
+                        case .none:
+                            throw BuilderError.noEnumCaseSet
+                        }
+                    }
+                    public class One: BuilderProtocol {
+                        public let i0: BuildableProperty<Int>
+                        public let i1: BuildableProperty<Double>
+                        public let i2: BuildableProperty<String>
+                        public required init() {
+                            i0 = BuildableProperty(name: "i0")
+                            i1 = BuildableProperty(name: "i1")
+                            i2 = BuildableProperty(name: "i2")
+                        }
+                        @discardableResult
+                        public func set(i0: Int) -> One {
+                            self.i0.set(value: i0)
+                            return self
+                        }
+                        @discardableResult
+                        public func set(i1: Double) -> One {
+                            self.i1.set(value: i1)
+                            return self
+                        }
+                        @discardableResult
+                        public func set(i2: String) -> One {
+                            self.i2.set(value: i2)
+                            return self
+                        }
+                        public func build() throws -> Foo {
+                            return try .one(i0: i0.build(), i1: i1.build(), i2: i2.build())
+                        }
+                    }
+                    private enum BuilderCases {
+                        case one(One)
+                    }
+                }
+            }
+            extension Foo: Buildable {
+            }
+            """, macros: testMacros)
+    }
 }
