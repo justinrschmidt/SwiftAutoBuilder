f064ea8126baa7d95ebdb0acd63b030bc7db8e0b
schimdt.cpp <schmidt.cpp@gmail.com>
2023-09-28 15:03:34 -0400

    delete unneeded string extension

--- Sources/AutoBuilderMacros/Extensions/StringExtension.swift
+++
@@ -1,8 +0,0 @@
-extension String {
-    var capitalized: String {
-        guard !isEmpty else {
-            return ""
-        }
-        return first!.uppercased() + self[index(after: startIndex)...]
-    }
-}
