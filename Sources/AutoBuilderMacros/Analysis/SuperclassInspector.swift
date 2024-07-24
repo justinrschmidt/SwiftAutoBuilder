2cf0b293e49a73d00ead9c1838297cefd9194990
schimdt.cpp <schmidt.cpp@gmail.com>
2024-07-24 17:57:55 -0400

    create SuperclassInspector which extracts declarations needed for calling a superclass's initializer

---
+++ Sources/AutoBuilderMacros/Analysis/SuperclassInspector.swift
@@ -0,0 +1,74 @@
+import SwiftSyntax
+import SwiftSyntaxBuilder
+
+/// Contains methods relating to analyzing the `Buildable` attribute for a superclass initializer and extracting the
+/// declarations relating to the superclass's initializer.
+struct SuperclassInspector {
+
+    /// Checks if the given attribute list contains a `Buildable` attribute and if that attribute has the
+    /// `superclassInitializer` parameter.
+    /// - Parameters:
+    ///   - attributeList: The attribute list to check.
+    /// - Returns: `true` if the attribute list contains a `Buildable` attribute with the `superclassInitializer`
+    /// parameter, `false` otherwise.
+    static func hasSuperclassArgument(in attributeList: AttributeListSyntax) -> Bool {
+        guard let buildableAttribute = getBuildableAttribute(in: attributeList) else { return false }
+        return getSuperclassInitializerExpr(in: buildableAttribute) != nil
+    }
+
+    /// Returns a `SuperclassInitializer` that contains the declarations relating to a superclass's initializer.
+    /// - Parameters:
+    ///   - decl: The class declaration that has the `Buildable` attribute with a `superclassInitializer` for the
+    ///   superclass's initializer declarations.
+    /// - Returns: A `SuperclassInitializer` populated with the superclass's initalizer declarations, if it exists,
+    /// `nil` if no superclass initalizer declaration is found.
+    static func getSuperclassInitializer(from decl: ClassDeclSyntax) -> SuperclassInitializer? {
+        guard let buildableAttribute = getBuildableAttribute(in: decl.attributes) else { return nil }
+        guard let attributeName = buildableAttribute.attributeName.as(IdentifierTypeSyntax.self) else { return nil }
+        let initExpr = getSuperclassInitializerExpr(in: buildableAttribute)
+        return SuperclassInitializer(
+            genericArgumentTypes: attributeName.genericArgumentClause?.arguments.map({ $0.argument }),
+            initializerBase: initExpr?.base,
+            initializerName: initExpr?.declName.baseName,
+            parameterLabels: initExpr?.declName.argumentNames?.arguments.map({ $0.name }))
+    }
+
+    private static func getBuildableAttribute(in attributeList: AttributeListSyntax) -> AttributeSyntax? {
+        for listElement in attributeList {
+            guard case let .attribute(attributeSyntax) = listElement else { continue }
+            guard let identifier = attributeSyntax.attributeName.as(IdentifierTypeSyntax.self) else { continue }
+            if identifier.name.text == "Buildable" {
+                return attributeSyntax
+            }
+        }
+        return nil
+    }
+
+    private static func getSuperclassInitializerExpr(in attribute: AttributeSyntax) -> MemberAccessExprSyntax? {
+        guard case let .argumentList(arguments) = attribute.arguments else { return nil }
+        for labeledExpr in arguments {
+            guard labeledExpr.label?.text == "superclassInitializer" else { continue }
+            guard let expression = labeledExpr.expression.as(MemberAccessExprSyntax.self) else { continue }
+            return expression
+        }
+        return nil
+    }
+
+    /// Contains the declarations in a `Buildable` attribute for a superclass's initializer.
+    struct SuperclassInitializer {
+
+        /// The list of generic arguments for the attribute.
+        let genericArgumentTypes: [TypeSyntax]?
+
+        /// The base expression for the initializer. Appears on the left side of the `.`. Typically, this is the
+        /// superclass's name.
+        let initializerBase: ExprSyntax?
+
+        /// The function base name of the initializer. Appears on the left side of the `.` and left of the `(`.
+        /// Typically, this is `init`.
+        let initializerName: TokenSyntax?
+
+        /// The parameter labels used in the initializer. Includes wildcards for parameters that do not have labels.
+        let parameterLabels: [TokenSyntax]?
+    }
+}
---
+++ Tests/AutoBuilderMacroTests/Analysis/SuperclassInspectorTests.swift
@@ -0,0 +1,153 @@
+import SwiftParser
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import XCTest
+@testable import AutoBuilderMacros
+
+final class SuperclassInspectorTests: XCTestCase {
+
+    // MARK: - Has Superclass Argument
+
+    func testHasSuperclassArgument_struct() throws {
+        let structDecl = try StructDeclSyntax("@Buildable(superclassInitializer: Foo.init) struct Bar") {}
+        XCTAssertTrue(SuperclassInspector.hasSuperclassArgument(in: structDecl.attributes))
+    }
+
+    func testHasSuperclassArgument_enum() throws {
+        let enumDecl = try EnumDeclSyntax("@Buildable(superclassInitializer: Foo.init) enum Bar") {}
+        XCTAssertTrue(SuperclassInspector.hasSuperclassArgument(in: enumDecl.attributes))
+    }
+
+    func testHasSuperclassArgument_class() throws {
+        let classDecl = try ClassDeclSyntax("@Buildable(superclassInitializer: Foo.init) class Bar") {}
+        XCTAssertTrue(SuperclassInspector.hasSuperclassArgument(in: classDecl.attributes))
+    }
+
+    func testDoesNotHaveSuperclassArgument_struct() throws {
+        let structDecl = try StructDeclSyntax("@Buildable struct Bar") {}
+        XCTAssertFalse(SuperclassInspector.hasSuperclassArgument(in: structDecl.attributes))
+    }
+
+    func testDoesNotHaveSuperclassArgument_enum() throws {
+        let enumDecl = try EnumDeclSyntax("@Buildable enum Bar") {}
+        XCTAssertFalse(SuperclassInspector.hasSuperclassArgument(in: enumDecl.attributes))
+    }
+
+    func testDoesNotHaveSuperclassArgument_class() throws {
+        let classDecl = try ClassDeclSyntax("@Buildable class Bar") {}
+        XCTAssertFalse(SuperclassInspector.hasSuperclassArgument(in: classDecl.attributes))
+    }
+
+    // MARK: - Get Superclass Initializer
+
+    func testGetSuperclassInitializer_withArguments() throws {
+        let classDecl = try ClassDeclSyntax("@Buildable<Foo, Int, Double>(superclassInitializer: Foo.init(a:_:)) class Bar") {}
+
+        let initializer = SuperclassInspector.getSuperclassInitializer(from: classDecl)
+
+        XCTAssertNotNil(initializer)
+
+        XCTAssertNotNil(initializer!.genericArgumentTypes)
+        let argumentTypes = initializer!.genericArgumentTypes!
+        XCTAssertEqual(argumentTypes.count, 3)
+        XCTAssertEqual(argumentTypes[0].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Foo"))
+        XCTAssertEqual(argumentTypes[1].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Int"))
+        XCTAssertEqual(argumentTypes[2].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Double"))
+
+        XCTAssertNotNil(initializer!.initializerBase)
+        XCTAssertEqual(
+            initializer!.initializerBase!.as(DeclReferenceExprSyntax.self)?.baseName.tokenKind, .identifier("Foo"))
+
+        XCTAssertNotNil(initializer!.initializerName)
+        XCTAssertEqual(initializer!.initializerName!.tokenKind, .keyword(.`init`))
+
+        XCTAssertNotNil(initializer!.parameterLabels)
+        XCTAssertEqual(initializer!.parameterLabels!.count, 2)
+        XCTAssertEqual(initializer!.parameterLabels![0].tokenKind, .identifier("a"))
+        XCTAssertEqual(initializer!.parameterLabels![1].tokenKind, .wildcard)
+    }
+
+    func testGetSuperclassInitializer_noArguments() throws {
+        let classDecl = try ClassDeclSyntax("@Buildable<Foo>(superclassInitializer: Foo.init) class Bar") {}
+
+        let initializer = SuperclassInspector.getSuperclassInitializer(from: classDecl)
+
+        XCTAssertNotNil(initializer)
+
+        XCTAssertNotNil(initializer!.genericArgumentTypes)
+        XCTAssertEqual(initializer!.genericArgumentTypes!.count, 1)
+        XCTAssertEqual(
+            initializer!.genericArgumentTypes![0].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Foo"))
+
+        XCTAssertNotNil(initializer!.initializerBase)
+        XCTAssertEqual(
+            initializer!.initializerBase!.as(DeclReferenceExprSyntax.self)?.baseName.tokenKind, .identifier("Foo"))
+
+        XCTAssertNotNil(initializer!.initializerName)
+        XCTAssertEqual(initializer!.initializerName!.tokenKind, .keyword(.`init`))
+
+        XCTAssertNil(initializer!.parameterLabels)
+    }
+
+    func testGetSuperclassInitializer_missingGenericArguments() throws {
+        let classDecl = try ClassDeclSyntax("@Buildable(superclassInitializer: Foo.init) class Bar") {}
+
+        let initializer = SuperclassInspector.getSuperclassInitializer(from: classDecl)
+
+        XCTAssertNotNil(initializer)
+
+        XCTAssertNil(initializer!.genericArgumentTypes)
+
+        XCTAssertNotNil(initializer!.initializerBase)
+        XCTAssertEqual(
+            initializer!.initializerBase!.as(DeclReferenceExprSyntax.self)?.baseName.tokenKind, .identifier("Foo"))
+
+        XCTAssertNotNil(initializer!.initializerName)
+        XCTAssertEqual(initializer!.initializerName!.tokenKind, .keyword(.`init`))
+
+        XCTAssertNil(initializer!.parameterLabels)
+    }
+
+    func testGetSuperclassInitializer_functionNotInit() throws {
+        let classDecl = try ClassDeclSyntax("@Buildable<Foo>(superclassInitializer: Foo.baz) class Bar") {}
+
+        let initializer = SuperclassInspector.getSuperclassInitializer(from: classDecl)
+
+        XCTAssertNotNil(initializer)
+
+        XCTAssertNotNil(initializer!.genericArgumentTypes)
+        XCTAssertEqual(initializer!.genericArgumentTypes!.count, 1)
+        XCTAssertEqual(
+            initializer!.genericArgumentTypes![0].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Foo"))
+
+        XCTAssertNotNil(initializer!.initializerBase)
+        XCTAssertEqual(
+            initializer!.initializerBase!.as(DeclReferenceExprSyntax.self)?.baseName.tokenKind, .identifier("Foo"))
+
+        XCTAssertNotNil(initializer!.initializerName)
+        XCTAssertEqual(initializer!.initializerName!.tokenKind, .identifier("baz"))
+
+        XCTAssertNil(initializer!.parameterLabels)
+    }
+
+    func testGetSuperclassInitializer_withClosure() throws {
+        let classDecl = try ClassDeclSyntax("@Buildable<Foo, Int, Double>(superclassInitializer: { (a, b) in return c }) class Bar") {}
+
+        let initializer = SuperclassInspector.getSuperclassInitializer(from: classDecl)
+
+        XCTAssertNotNil(initializer)
+
+        XCTAssertNotNil(initializer!.genericArgumentTypes)
+        let argumentTypes = initializer!.genericArgumentTypes!
+        XCTAssertEqual(argumentTypes.count, 3)
+        XCTAssertEqual(argumentTypes[0].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Foo"))
+        XCTAssertEqual(argumentTypes[1].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Int"))
+        XCTAssertEqual(argumentTypes[2].as(IdentifierTypeSyntax.self)?.name.tokenKind, .identifier("Double"))
+
+        XCTAssertNil(initializer!.initializerBase)
+
+        XCTAssertNil(initializer!.initializerName)
+
+        XCTAssertNil(initializer!.parameterLabels)
+    }
+}
